4008ee1f6e05cb902a29b86dc9c06fd7b8c989ff
Oliver Koerber <oliver.koerber@it-forum.de>
2025-09-11 14:23:23 +0200

    Step 2: Create Model

--- webapp/index.js
+++ webapp/index.js
@@ -1,9 +1,16 @@
-sap.ui.require(["sap/m/Text"], function (Text) {
-  "use strict";
+sap.ui.require(
+  ["sap/m/Text", "sap/ui/model/json/JSONModel"],
+  function (Text, JSONModel) {
+    "use strict";
 
-  sap.ui.getCore().attachInit(function () {
-    new Text({
-      text: "Hi, my name is... Oliver",
-    }).placeAt("content");
-  });
-});
+    var oModel = sap.ui.getCore().attachInit(function () {
+      var oModel = new JSONModel({
+        greetingText: "Hi, my name is... Oliver",
+      });
+
+      new Text({
+        text: "Hi, my name is... Oliver",
+      }).placeAt("content");
+    });
+  },
+);
