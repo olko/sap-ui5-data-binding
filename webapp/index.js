df3cd097da845b35bdc79a3865dc07d800b32fc2
Oliver Koerber <oliver.koerber@it-forum.de>
2025-09-12 13:57:59 +0200

    Step 6: Resource Models

--- webapp/index.js
+++ webapp/index.js
@@ -1,10 +1,12 @@
 sap.ui.require(
-  ["sap/m/Text",
+  [
+   "sap/m/Text",
    "sap/ui/model/json/JSONModel", 
    "sap/ui/core/mvc/XMLView",
-   "sap/ui/model/BindingMode"
+   "sap/ui/model/BindingMode",
+   "sap/ui/model/resource/ResourceModel"
   ],
-  function (Text, JSONModel, XMLView, BindingMode) {
+  function (Text, JSONModel, XMLView, BindingMode, ResourceModel) {
     "use strict";
 
     sap.ui.getCore().attachInit(function () {
@@ -14,8 +16,15 @@ sap.ui.require(
         lastName: "Körber",
         enabled: true
       });
-      oModel.setDefaultBindingMode(BindingMode.OneWay);
+
+      var oResourceModel = new ResourceModel({
+        bundleName: "sap.ui.demo.db.i18n.i18n",
+        supportedLocales: ["", "de"],
+        fallbackLocale: ""
+      })
+      // oModel.setDefaultBindingMode(BindingMode.OneWay);
       sap.ui.getCore().setModel(oModel);
+      sap.ui.getCore().setModel(oResourceModel, "i18n");
 
       new XMLView({
         viewName: "sap.ui.demo.db.view.App"
